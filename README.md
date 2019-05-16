5f7e359ebc35066272ef307fe651899737c36839
Milene Jezuz <milene.guimaraes@b2wdigital.com>
2019-05-16 12:54:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# shakeproject by Jessica Marriel
---
+++ src/main/java/com/shake/shake/ShakeApplication.java
@@ -0,0 +1,14 @@
+package com.shake.shake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ShakeApplication.class, args);
+	}
+
+}
